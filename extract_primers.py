5875d4c8ad5c6150bfb929f7ab8df7ce042b6edf
billyfournier <billyfournier2000@yahoo.com>
2016-04-21 16:32:27 -0500

    added RE version from William Walters

--- extract_primers.py
+++ extract_primers.py
@@ -5,10 +5,18 @@
 __author__ = "Billy Fournier"
 __license__ = "GPL"
 __email__ = "billyfournier2000@yahoo.com"
+__credits__ = "William Walters"
+""" This script is an adaptation of William Walters work with
+    the qiime project.
+"""
 
 from string import upper
 from itertools import product
 from optparse import OptionParser
+
+from skbio.sequence import DNA
+from skbio.parse.sequences import parse_fastq
+from skbio.format.sequences import format_fastq_record
 
 parser = OptionParser()
 parser.add_option("-f", "--forward_read", dest="forward_file",
@@ -16,9 +24,9 @@ parser.add_option("-f", "--forward_read", dest="forward_file",
 parser.add_option("-r", "--reverse_read", dest="reverse_file",
                         help="(default) read2.fastq", default="read2.fastq")
 parser.add_option("-o", "--forward_out", dest="forward_out",
-                        help="name of forward output file", default="out1.fastq")
+                        help="name of forward output file", default="rd1.fastq")
 parser.add_option("-O", "--reverse_out", dest="reverse_out",
-                        help="name of reverse output file", default="out2.fastq")
+                        help="name of reverse output file", default="rd2.fastq")
 parser.add_option("-m", "--mapping_file", dest="mapping_file",
                         default="mappingfile.txt", help="mappingfile for fastq")
 
@@ -38,6 +46,13 @@ if options.mapping_file is None:
 # 	print "       This is the name of the output"
 # 	exit(-1)
 
+""" Creates all combinations of a primer sequence with a known variable base/s.
+
+EXAMPLE:
+A primer "ATCGN" has 4 variations at the last position so 4 bases can be
+represented. Those primers would be:
+"ATCGA"     "ATCGT"     "ATCGC"     "ATCGG"
+"""
 
 def gen_primer_list(mapping_primer):
     var = {'A': 'A', 'T': 'T', 'G': 'G', 'C': 'C', 'R': 'AG', 'Y': 'CT',
@@ -68,12 +83,15 @@ def reverse_complement(seq):
 
 
 def get_primers(mapping_file):
-    """ Generates and returns all forward and reverse primer combinations 
+    """ Generates and returns all forward and reverse primer combinations
     from the mapping file.
     mapping_data:  the sequenced data's mapping file
     Will raise error if either the LinkerPrimerSequence or ReversePrimer fields
         are not present
     """
+    iupac = {'A': 'A', 'T': 'T', 'G': 'G', 'C': 'C', 'R': 'AG', 'Y': 'CT',
+             'S': 'GC', 'W': 'AT', 'K': 'GT', 'M': 'AC', 'B': 'CGT',
+             'D': 'AGT', 'H': 'ACT', 'V': 'ACG', 'N': 'ACGT'}
     # processing mapping_file into header and mapping_data
     with open(mapping_file) as mappingfile:
         header = mappingfile.readline().split()
@@ -94,91 +112,74 @@ def get_primers(mapping_file):
         raise IndexError(("Mapping file is missing ReversePrimer field."))
 
     raw_forward_primers = set([])
+    raw_forward_rc_primers = set([])
     raw_reverse_primers = set([])
+    raw_reverse_rc_primers = set([])
+
     for line in mapping_data:
-        # Split on commas to handle pool of primers
+# Split on commas to handle pool of primers
         if len(line) >= rev_primer_ix:
             raw_forward_primers.update([upper(primer).strip() for
                                     primer in line[primer_ix].split(',')])
+            # raw_forward_rc_primers.update([str(DNA(primer).rc()) for
+            #                            primer in raw_forward_primers])
             raw_reverse_primers.update([upper(primer).strip() for
                                     primer in line[rev_primer_ix].split(',')])
+            # raw_reverse_rc_primers.update([str(DNA(primer).rc()) for
+            #                            primer in raw_reverse_primers])
+
+            print raw_forward_primers
 
     if not raw_forward_primers:
         raise ValueError(("No forward primers detected in mapping file."))
     if not raw_reverse_primers:
         raise ValueError(("No reverse primers detected in mapping file."))
 
+# Finding the forward primers, or rc of reverse primers indicates forward
+# read. Finding the reverse primer, or rc of the forward primers, indicates
+# the reverse read, so these sets are merged.
+    # raw_forward_primers.update(raw_reverse_rc_primers)
+    # raw_reverse_primers.update(raw_forward_rc_primers)
+
     forward_primers = set([])
     reverse_primers = set([])
+    # forward_rc_primers = set([])
+    # reverse_rc_primers = set([])
+
     for sequence in raw_forward_primers:
         forward_primers |= set(gen_primer_list(sequence))
     for sequence in raw_reverse_primers:
         reverse_primers |= set(gen_primer_list(sequence))
-
+    # for sequence in raw_forward_rc_primers:
+    #     forward_rc_primers |= set(gen_primer_list(sequence))
+    # for sequence in raw_reverse_rc_primers:
+    #     reverse_rc_primers |= set(gen_primer_list(sequence))
     return forward_primers, reverse_primers
 
+import re
+def make_regex(primer_list):
+# Converted a list of tuples to a list of strings
+    primers = [''.join(primer) for primer in primer_list]
+    primers = re.compile("|".join(primers))
+    return primers
 
-# sequence identifier beginning format
-# @<instrument>:<run number>:
-def remove_primers(read_file,out_name,primers,tolerance):
-    is_sequence = False
-    seq_passed = False
-    seq_identifier = ''
-    seq_data = ''
-    qual_data = ''
-    # print index_file
-    with open(read_file) as input:
-        tmp_str = input.readline()
-        index = tmp_str.find(':')
-        instrument_id = tmp_str[0:index]
 
-    with open(read_file) as input, open(out_name,"w") as output:
-        for index, line in enumerate(input):
-            # write qual data & Marker for sequence data
+def remove_primers(input_fastq, output_fastq,primers):
+    count = 0
+    # USING regex list (Time 11m4)
+    with open(input_fastq) as read, open(output_fastq, "w") as out_seqs:
+        for label,seq,qual in parse_fastq(read):
+            start_slice = 0
+            if primers.search(seq):
+                start_slice = int(primers.search(seq).end())
+            curr_seq = seq[start_slice:]
+            curr_qual = qual[start_slice:]
+            if start_slice > 0:
+                formatted_fastq_line = format_fastq_record(label, curr_seq, curr_qual)
+                # print ("%s" % (formatted_fastq_line))
+                out_seqs.write("%s" % (formatted_fastq_line))
 
-            if instrument_id in line:
-                is_sequence = True
-                seq_identifier = line
-                if seq_passed:
-                    qual_data = qual_data[new_index:]
-                    output.write('+\n')
 
-                    # print ''.join(qual_data.split())
-                    output.write(qual_data)
-                seq_passed = False
-
-            # Marker for quality data and performs primer check
-            elif line.split() == ['+']:
-                is_sequence = False
-                for primer in forward_primers:
-                    new_index = seq_data.rfind(''.join(primer))
-                    if new_index is not -1:
-                        new_index = new_index + len(primer) - 1
-                        seq_data = seq_data[new_index:]
-                        if len(seq_data) > tolerance:
-                            # print ''.join(seq_identifier.split())
-                            # print ''.join(seq_data.split())
-                            output.write(seq_identifier)
-                            output.write(seq_data)
-                            seq_passed = True
-                            break
-
-            # Populate seq_data with sequence string
-            elif is_sequence:
-                seq_data = seq_data + line
-                qual_data = ''
-
-            # Populates qual_data & clears seq_data
-            elif not is_sequence:
-                qual_data = qual_data + line
-                seq_data = ''
-
-        if seq_passed:
-            qual_data = qual_data[new_index:]
-            # print "+"
-            # print ''.join(qual_data.split())
-            output.write('+\n')
-            output.write(qual_data)
 
 mapping_file = options.mapping_file
 forward_read = options.forward_file
@@ -187,8 +188,9 @@ forward_out = options.forward_out
 reverse_out = options.reverse_out
 
 forward_primers, reverse_primers = get_primers(mapping_file)
-# print reverse_primers
-for primer in list(forward_primers):
-    print ''.join(primer)
-remove_primers(forward_read, forward_out, forward_primers, 50)
-remove_primers(reverse_read, reverse_out, reverse_primers, 50)
+# print "BEGINNING ", forward_read
+# forward_primers = make_regex(forward_primers)
+# remove_primers(forward_read, forward_out, forward_primers)
+# print forward_read, " COMPLETE"
+# reverse_primers = make_regex(reverse_primers)
+# remove_primers(reverse_read, reverse_out, reverse_primers)
